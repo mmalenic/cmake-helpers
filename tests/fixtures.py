667fa49e2ca29d411095a04a8289efbac42348b4
Marko Malenic <mmalenic1@gmail.com>
2024-07-06 17:06:30 +1000

    test: remaining check_symbol tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,14 +1,36 @@
 from os.path import dirname, realpath
 from pathlib import Path
 from shutil import copytree, copy
+from subprocess import run
+from typing import Dict, Optional
 
 import pytest
 
 
-def setup_cmake_project(tmp_path, monkeypatch, test_directory) -> Path:
+def run_cmake_with_assert(capfd, contains_message: str, variables: Optional[Dict[str, str]] = None):
+    """
+    Run cmake with an expected assert message and additional variables to define.
+    """
+    command = "cmake . "
+    if variables is not None:
+        for key, value in variables.items():
+            command += f"-D {key}={value}"
+
+    run(command.split(), check=True)
+    out, _ = capfd.readouterr()
+    assert contains_message in out
+
+    run("cmake --build .".split(), check=True)
+    run("./cmake_helpers_test", check=True)
+
+
+def setup_cmake_project(tmp_path, monkeypatch, data_path) -> Path:
+    """
+    This fixture copies the requested test data into a tmp_dir for cmake to run.
+    """
     file_path = Path(dirname(realpath(__file__)))
 
-    copytree(file_path / test_directory, tmp_path, dirs_exist_ok=True)
+    copytree(file_path / "test_data" / data_path, tmp_path, dirs_exist_ok=True)
     copy(file_path.parent / "helpers.cmake", tmp_path)
 
     monkeypatch.chdir(tmp_path)
@@ -17,5 +39,5 @@ def setup_cmake_project(tmp_path, monkeypatch, test_directory) -> Path:
 
 
 @pytest.fixture
-def check_symbols(tmp_path, monkeypatch):
-    return setup_cmake_project(tmp_path, monkeypatch, "test_data/check_symbols")
+def check_symbol(tmp_path, monkeypatch) -> Path:
+    return setup_cmake_project(tmp_path, monkeypatch, "check_symbol")
