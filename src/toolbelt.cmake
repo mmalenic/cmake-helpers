c6674bbcfcfeb2a77d27df3e9433d3bbec3585f5
Marko Malenic <mmalenic1@gmail.com>
2024-09-28 12:12:16 +1000

    docs: update version in README.md

--- src/toolbelt.cmake
+++ src/toolbelt.cmake
@@ -31,7 +31,7 @@ commands are available after including with a :cmake:`toolbelt_` prefix:
    FetchContent_Declare(
         toolbelt
         GIT_REPOSITORY https://github.com/mmalenic/cmake-toolbelt
-        GIT_TAG v0.1.1
+        GIT_TAG v0.2.0
    )
    FetchContent_MakeAvailable(toolbelt)
 
